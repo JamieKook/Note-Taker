1e0ff0e9c1cf26112acdfd29935c9f29559550e2
Jamie <kookjc6@gmail.com>
2020-01-16 15:12:14 -0600

    added delete method to remove notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,7 +27,22 @@ module.exports = function (app) {
         res.json(newNote);  
     }); 
 
-    // app.delete("/api/notes/:id", function(req, res) {
-
-    // })
+    app.delete("/api/notes/:id", function(req, res) {
+        const chosenId= parseInt(req.params.id);
+        console.log(chosenId); 
+        fs.readFile("db/db.json", (err,data) =>{
+            if (err) throw err; 
+            notesArr = JSON.parse(data); 
+            for (let i = 0; i< notesArr.length; i++){
+                if (notesArr[i].id === chosenId){
+                    console.log(i); 
+                    notesArr.splice(i, 1); 
+                }
+            }
+            fs.writeFile("db/db.json", JSON.stringify(notesArr,null, 2), (err) =>{
+                if (err) throw err;   
+            }); 
+        }); 
+       
+    }); 
 }; 
