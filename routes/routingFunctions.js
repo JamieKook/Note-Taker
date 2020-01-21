6d876803bd2296b0550d80a1abdace9d7a89c170
Jamie <kookjc6@gmail.com>
2020-01-20 20:21:57 -0600

    worked on removing logic from fs in order to mock and test

--- routes/routingFunctions.js
+++ routes/routingFunctions.js
@@ -1,41 +1,87 @@
 const fs = require("fs"); 
+const util = require ("util"); 
 
-function postNotes(req, res) {
+const readFileAsync = util.promisify(fs.readFile); 
+
+async function postNotes(req, res) {
     let newNote= req.body; 
     let notesArr = []; 
-    fs.readFile("db/db.json", (err, data) => {
-        if (err) throw err; 
-        notesArr= JSON.parse(data); 
-        let isOldNote= false; 
-        //check to see if note is already saved and just being edited
-        for (let i=0; i<notesArr.length; i++){
-            if (notesArr[i].id === parseInt(newNote.id)){
-                //new code to handle this situation 
-                newNote.id = parseInt(newNote.id);  
-                notesArr.splice(i, 1, newNote); 
-                isOldNote= true; 
-            } 
-        }
-
-        if (!isOldNote){
-            //moved inside the for statement from previous code
-            let currentId= 0; 
-            if (notesArr.length > 0){
-                let currentLength= notesArr.length; 
-                currentId= notesArr[currentLength-1].id;
-            } else {
-                currentId= 0; 
-            }
-            newNote.id= currentId +1; 
-            notesArr.push(newNote);
-        }
-        
-        res.json(newNote); 
-        
-        fs.writeFile("db/db.json", JSON.stringify(notesArr,null, 2), (err) =>{
-            if (err) throw err;  
+    let Filedata=""; 
+    try {
+        await fs.readFile("db/db.json", "Utf8", (err,data)=>{
+            if (err) throw err; 
+            Filedata= data; 
         }); 
+
+        notesArr= JSON.parse(Filedata); 
+    let isOldNote= false; 
+    //check to see if note is already saved and just being edited
+    for (let i=0; i<notesArr.length; i++){
+        if (notesArr[i].id === parseInt(newNote.id)){
+            //new code to handle this situation 
+            newNote.id = parseInt(newNote.id);  
+            notesArr.splice(i, 1, newNote); 
+            isOldNote= true; 
+        } 
+    }
+
+    if (!isOldNote){
+        //moved inside the for statement from previous code
+        let currentId= 0; 
+        if (notesArr.length > 0){
+            let currentLength= notesArr.length; 
+            currentId= notesArr[currentLength-1].id;
+        } else {
+            currentId= 0; 
+        }
+        newNote.id= currentId +1; 
+        notesArr.push(newNote);
+    }
+    
+    res.json(newNote); 
+    
+    fs.writeFile("db/db.json", JSON.stringify(notesArr,null, 2), (err) =>{
+        if (err) throw err;  
     }); 
+
+    } catch (err) {
+        console.log(err); 
+    }
+
+    
+    // fs.readFile("db/db.json", (err, data) => {
+    //     if (err) throw err; 
+    //     notesArr= JSON.parse(data); 
+    //     let isOldNote= false; 
+    //     //check to see if note is already saved and just being edited
+    //     for (let i=0; i<notesArr.length; i++){
+    //         if (notesArr[i].id === parseInt(newNote.id)){
+    //             //new code to handle this situation 
+    //             newNote.id = parseInt(newNote.id);  
+    //             notesArr.splice(i, 1, newNote); 
+    //             isOldNote= true; 
+    //         } 
+    //     }
+
+    //     if (!isOldNote){
+    //         //moved inside the for statement from previous code
+    //         let currentId= 0; 
+    //         if (notesArr.length > 0){
+    //             let currentLength= notesArr.length; 
+    //             currentId= notesArr[currentLength-1].id;
+    //         } else {
+    //             currentId= 0; 
+    //         }
+    //         newNote.id= currentId +1; 
+    //         notesArr.push(newNote);
+    //     }
+        
+    //     res.json(newNote); 
+        
+    //     fs.writeFile("db/db.json", JSON.stringify(notesArr,null, 2), (err) =>{
+    //         if (err) throw err;  
+    //     }); 
+    // }); 
 }
 
 function getNotes(req, res){
@@ -64,4 +110,5 @@ function deleteNotes(req, res){
 
 module.exports = {"postNotes": postNotes, 
                 "getNotes": getNotes, 
-                "deleteNotes": deleteNotes}; 
+                "deleteNotes": deleteNotes,
+                "readFileAsync": readFileAsync}; 
--- test/routingFunctions.test.js
+++ test/routingFunctions.test.js
@@ -1,35 +1,40 @@
-// const { postNotes } = require("../routes/routingFunctions"); 
+const { postNotes, readFileAsync } = require("../routes/routingFunctions"); 
 
 // const fs = require("fs"); 
+// const util = require("util"); 
 // jest.mock("fs"); 
+// jest.mock("util"); 
+// jest.mock(readFileAsync); 
+const mockRequest = (sessionData, body) => ({
+    session: { data: sessionData },
+    body
+    });
 
-// const mockRequest = (sessionData, body) => ({
-//     session: { data: sessionData },
-//     body
-//     });
+const mockResponse = () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+    };
 
-// const mockResponse = () => {
-//     const res = {};
-//     res.status = jest.fn().mockReturnValue(res);
-//     res.json = jest.fn().mockReturnValue(res);
-//     return res;
-//     };
-
-// describe("Post Functions", () => {
-//     it("the posted note's id is not changed if the note is being edited (it matches the id of a note in the file)", async () => {
-//         let note= {title: "test edit",
-//             text: "edited text",
-//             id: 3}; 
-//         let req= mockRequest(
-//             {},
-//             {note}
-//         ); 
-//         let res= mockResponse(); 
-//         let data = '[{"title":"test","text":"test text","id":1},{"title":"test","text":"test text","id":2},{"title":"test","text":"test text","id":3}]'; 
-//         fs.readFile.mockReturnValue(data);
-//         await postNotes(req, res); 
-//         expect(res.json).toHaveBeenCalledWith(note);  
-//     }); 
-// }); 
+describe("Post Functions", () => {
+    it("the posted note's id is not changed if the note is being edited (it matches the id of a note in the file)", async () => {
+        let note= {title: "test edit",
+            text: "edited text",
+            id: 3}; 
+        let req= mockRequest(
+            {},
+            {note}
+        ); 
+        let res= mockResponse();
+        
+        let data = '[{"title":"test","text":"test text","id":1},{"title":"test","text":"test text","id":2},{"title":"test","text":"test text","id":3}]'; 
+        // fs.readFile.mockReturnValue(data);
+        const spy = jest.spyOn(readFileAsync); 
+        spy.mockReturnValue(data); 
+        await postNotes(req, res); 
+        expect(res.json).toHaveBeenCalledWith(note);  
+    }); 
+}); 
 
 //Doesn't work- mock out file system but need it to run to test logic
